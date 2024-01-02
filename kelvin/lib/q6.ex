bcfc8771e5ceab69c1d4de7513c7e67687bb2e91
Kelvin Marques <115046013+iKelvinDev@users.noreply.github.com>
2024-01-02 00:19:53 +0000

    feat: Ler n pares matrícula nome e guardar em dicionário

---
+++ kelvin/lib/q6.ex
@@ -0,0 +1,33 @@
+defmodule Q6 do
+
+  def ler_e_armazenar(n) do
+    matriculas_nomes = ler_matriculas_nomes(n)
+    dicionario = Enum.into(matriculas_nomes, %{})
+    IO.inspect(dicionario)
+  end
+
+  defp ler_matriculas_nomes(n) do
+    IO.puts("Digite os pares matrícula/nome (um por linha): ")
+    Enum.map(1..n, fn _ ->
+      {matricula, nome} = ler_matricula_nome()
+      {matricula, nome}
+    end)
+  end
+
+  defp ler_matricula_nome do
+    IO.puts("Matrícula: ")
+    matricula = IO.gets("") |> String.trim()
+
+    IO.puts("Nome:")
+    nome = IO.gets("") |> String.trim()
+
+    {matricula, nome}
+  end
+end
+
+IO.puts("Digite a quantidade de pares matrícula/nome: ")
+quantidade = IO.gets("") |> String.trim() |> String.to_integer()
+
+Q6.ler_e_armazenar(quantidade)
+
+end
